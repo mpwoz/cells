c0101c7a3f0321475ed247de344aaca5b1cd9775
mpwoz <martin.woz@gmail.com>
2025-06-25 16:43:31 -0700

    fix: remove annoying music

--- src/demo/level.rs
+++ src/demo/level.rs
@@ -23,7 +23,7 @@ impl FromWorld for LevelAssets {
     fn from_world(world: &mut World) -> Self {
         let assets = world.resource::<AssetServer>();
         Self {
-            music: assets.load("audio/music/Fluffing A Duck.ogg"),
+            music: assets.load("audio/music/Short_Silent,_Empty_Audio.ogg"),
         }
     }
 }
