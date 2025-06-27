4ffede6c1cb610368ecc4c26dbefb5c3e715fe2a
mpwoz <martin.woz@gmail.com>
2025-06-27 09:01:28 -0700

    fix: event-based eating without despawn errors

--- src/demo/eating.rs
+++ src/demo/eating.rs
@@ -1,17 +1,23 @@
+use std::collections::HashSet;
 use crate::demo::cell_bundle::{CellCreature, CellSize};
 use avian2d::prelude::*;
 use bevy::prelude::*;
 
 pub fn plugin(app: &mut App) {
-    app
-        .add_observer(detect_cell_collisions)
-        .add_systems(Update, clean_up_eaten_cells)
+    app.add_observer(detect_cell_collisions)
+        .add_observer(clean_up_eaten_cells)
+        .init_resource::<CollisionsAlreadyProcessedCache>()
         .register_type::<EatenBy>();
 }
 
-#[derive(Component, Reflect)]
+#[derive(Event, Reflect)]
 pub struct EatenBy {
-    eater: Entity,
+    pub eater: Entity,
+}
+
+#[derive(Resource, Default)]
+struct CollisionsAlreadyProcessedCache {
+    entities: HashSet<Entity>,
 }
 
 // System to detect collisions between cells
@@ -19,6 +25,7 @@ fn detect_cell_collisions(
     trigger: Trigger<OnCollisionStart>,
     mut commands: Commands,
     cells: Query<(Entity, &CellSize), With<CellCreature>>,
+    mut cache: ResMut<CollisionsAlreadyProcessedCache>,
 ) {
     // Check if both entities have Cell components
     if let (Ok((e1, CellSize(s1))), Ok((e2, CellSize(s2)))) =
@@ -27,16 +34,17 @@ fn detect_cell_collisions(
         // Determine which cell eats which (larger eats smaller)
         let (eater, eaten) = if s1 > s2 { (e1, e2) } else { (e2, e1) };
 
-        if let Ok(mut ec) = commands.get_entity(eaten) {
-            ec.insert(EatenBy { eater });
+        if !cache.entities.contains(&eaten) && let Ok(mut ec) = commands.get_entity(eaten) {
+            ec.trigger(EatenBy { eater });
+            cache.entities.insert(eaten.clone());
         }
     }
 }
 
-fn clean_up_eaten_cells(eaten: Query<Entity, With<EatenBy>>, mut commands: Commands) {
-    for entity in eaten.iter() {
-        if let Ok(mut ec) = commands.get_entity(entity) {
-            ec.despawn();
-        }
+fn clean_up_eaten_cells(eaten: Trigger<EatenBy>, mut commands: Commands, mut cache: ResMut<CollisionsAlreadyProcessedCache>) {
+    let entity = eaten.target();
+    if let Ok(mut ec) = commands.get_entity(entity) {
+        ec.despawn();
+        cache.entities.remove(&entity);
     }
 }
